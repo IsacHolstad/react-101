c3f9e49b82ad35179edeee5a9c2e26c9178ef162
IsacHolstad <wisac22@gmail.com>
2023-02-22 22:50:15 +0100

    changed ahref link

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,7 +10,7 @@ function Content(props) {
         <div>
             <ul>
                 <h3>Do you like frontend?</h3>
-                <a style={buttonStyling} href={"https://www.google.com/search?q=react+library&sxsrf=AJOqlzWI3FIDO5EbyMohhEeoDt1cDeGR-w%3A1677098560859&ei=QH72Y8yUNNGSrgS-hJ2YDw&ved=0ahUKEwjMtOT__qn9AhVRiYsKHT5CB_MQ4dUDCA4&uact=5&oq=react+library&gs_lcp=Cgxnd3Mtd2l6LXNlcnAQAzIFCAAQgAQyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywEyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywEyCAgAEIAEEMsBMggIABCABBDLATIICAAQgAQQywE6CggAEEcQ1gQQsAM6BwgAELADEEM6DQguEMcBENEDELADEEM6BAgjECc6CQgAEBYQHhDxBDoGCAAQFhAeOgsIABCABBCxAxCDAUoECEEYAFCWBFipN2CWOGgBcAF4AIABUogB6guSAQIyM5gBAKABAcgBCsABAQ&sclient=gws-wiz-serp"}>Click
+                <a style={buttonStyling} href={"https://parade.com/.image/t_share/MTkwNTgxMTA1NDAxOTk2NDEz/laughing-horse.jpg"}>Click
                     Here</a>
                 <p>React has been designed from the start for gradual adoption, and you can use as little or as much
                     React as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML
