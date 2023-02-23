f5cf058a38b76c837c86980bcd357324cc99e484
IsacHolstad <wisac22@gmail.com>
2023-02-23 15:00:57 +0100

    voter.js file made

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Content from "./components/Content";
 import ReactText from "./components/ReactText";
 import VoteForJhonny from "./components/VoteForJhonny";
 import VoteForMorten from"./components/VoteForMorten"
+import Counter from "./components/Voter";
 
 function App() {
   return (
@@ -25,17 +26,16 @@ function App() {
             textAlign="center"
         />
         <hr/>
-      <h3>Jhonny Votes: {0}</h3>
+      <h3 initialCount={0}>Jhonny Votes: </h3>
       <img className={"johnny-img"}
              src={"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"}
              alt="img of some guy"/>
         <VoteForJhonny/>
-      <h3>Morten Votes: {0}</h3>
+      <h3 initialCount={0}>Morten Votes: </h3>
       <img className={"morten-img"}
              src={"https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo="}
              alt="second img of some guy"/>
         <VoteForMorten/>
-
     </div>
   );
 }
