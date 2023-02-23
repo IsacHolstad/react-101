a90023cd16212390f7cbcf5fdb421decccca7653
IsacHolstad <wisac22@gmail.com>
2023-02-23 14:45:16 +0100

    vote conter in h3

--- src/App.js
+++ src/App.js
@@ -25,15 +25,17 @@ function App() {
             textAlign="center"
         />
         <hr/>
-        <img className={"johnny-img"}
+      <h3>Jhonny Votes: {0}</h3>
+      <img className={"johnny-img"}
              src={"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"}
              alt="img of some guy"/>
         <VoteForJhonny/>
-
-        <img className={"morten-img"}
+      <h3>Morten Votes: {0}</h3>
+      <img className={"morten-img"}
              src={"https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo="}
              alt="second img of some guy"/>
         <VoteForMorten/>
+
     </div>
   );
 }
