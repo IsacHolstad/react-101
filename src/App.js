e73b798c8c619cbdd94853ae90b537dc6eac4df1
IsacHolstad <wisac22@gmail.com>
2023-02-23 14:28:14 +0100

    added image of some guy to make voting system

--- src/App.js
+++ src/App.js
@@ -23,6 +23,8 @@ function App() {
             textAlign="center"
         />
         <hr/>
+        <img className={"johnny-img"} src={"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"} alt="img of some guy"/>
+
 
     </div>
   );
