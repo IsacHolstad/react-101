108d06b5fae17086e60ef4c2013a62a1467d6764
IsacHolstad <wisac22@gmail.com>
2023-02-24 12:42:21 +0100

    clean list products

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Heading from "./components/Heading";
 import Paragraph from "./components/Paragraph";
 import Content from "./components/Content";
 import ReactText from "./components/ReactText";
-import Counter from "./components/Voter";
+//import Counter from "./components/Voter";
 const proudcts = [
     {id: 0, name:"Milk", price: "10000$"},
     {id:1, name: "Mjølk", price: "10000$"},
     {id:2, name:"Candy", price: "10000$"},
     {id: 3, name:"Bread", price: "10000$"},
-    {id: 4, name:"Spray", price: "10000$"}
+    {id: 4, name:"Spray", price: "10000$"},
+    {id: 5, name:"PepsiMax", price: "1000434330$"}
 
 ];
 
@@ -32,12 +33,12 @@ function App() {
             textAlign="center"
         />
         <hr/>
-      <h3 initialCount={0}>Jhonny Votes:</h3>
+      <h3>Jhonny Votes:</h3>
       <img className={"johnny-img"}
              src={"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cmFuZG9tJTIwcGVvcGxlfGVufDB8fDB8fA%3D%3D&w=1000&q=80"}
              alt="img of some guy"/>
         <button>Vote For Jhonny</button>
-      <h3 initialCount={0}>Morten Votes:</h3>
+      <h3>Morten Votes:</h3>
       <img className={"morten-img"}
              src={"https://media.istockphoto.com/id/1200677760/photo/portrait-of-handsome-smiling-young-man-with-crossed-arms.jpg?s=612x612&w=0&k=20&c=g_ZmKDpK9VEEzWw4vJ6O577ENGLTOcrvYeiLxi8mVuo="}
              alt="second img of some guy"/>
@@ -50,8 +51,9 @@ function App() {
       <hr/>
         <ul>
             {proudcts.map((product => (
-                <li>
+                <li key={product.id}>
                     {product.name}
+                    <br/>
                     {product.price}
                 </li>
             )))}
