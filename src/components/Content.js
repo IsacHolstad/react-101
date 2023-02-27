52ea3e32bcb3459231c7383d2d4948d91e26eb8c
IsacHolstad <wisac22@gmail.com>
2023-02-27 18:17:30 +0100

    button styling for a href

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,7 +10,7 @@ function Content(props) {
         <div>
             <ul>
                 <h3>Do you like frontend?</h3>
-                <a style={buttonStyling} href={"https://parade.com/.image/t_share/MTkwNTgxMTA1NDAxOTk2NDEz/laughing-horse.jpg"}>Click
+                <a style={buttonStyling} className="aHrefButton" href={"https://parade.com/.image/t_share/MTkwNTgxMTA1NDAxOTk2NDEz/laughing-horse.jpg"}>Click
                     Here</a>
                 <p>React has been designed from the start for gradual adoption, and you can use as little or as much
                     React as you need. Whether you want to get a taste of React, add some interactivity to a simple HTML
